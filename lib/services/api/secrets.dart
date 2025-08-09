85bdeb545095e1a47266710a20e17e60ce6d066d
ziehro <56759812+ziehro@users.noreply.github.com>
2025-08-08 18:25:15 -0600

    Delete lib/services/api/secrets.dart

--- lib/services/api/secrets.dart
+++
@@ -1,7 +0,0 @@
-// lib/services/api/secrets.dart
-
-// Replace with your actual API keys
-const String geminiApiKey = 'YOUR_GEMINI_API_KEY';
-const String googleApiKey = 'YOUR_GOOGLE_API_KEY';
-const String googleSearchId = 'YOUR_Google Search_ENGINE_ID';
-const String openAIApiKey = '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';
